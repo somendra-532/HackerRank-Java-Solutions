a0e235943b9a16752fa23caf7bbe6e5b92cf453f
somendra <somendraaguru143@gmail.com>
2026-08-01 18:45:41 +0530

    Java LoopsII.java

---
+++ 07_Java Loops II.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class JavaLoopsII {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+            int sum = a;
+
+            for (int j = 0; j < n; j++) {
+                sum = sum + (int) Math.pow(2, j) * b;
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
